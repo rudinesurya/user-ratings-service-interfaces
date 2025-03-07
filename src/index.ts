f534e8af7910d64a30a9c67f3a368066d0a49dc2
rudinesurya <rudinesurya@gmail.com>
2025-03-07 16:32:44 +0000

    update

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './user-rating-create-response.interface';
-export * from './user-rating-delete-response.interface';
-export * from './user-rating-update-response.interface';
-export * from './user-rating-update.interface';
-export * from './user-rating.interface';
-export * from './user-ratings-search-response.interface';
